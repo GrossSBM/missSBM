3b2842096ea17f11fcee69f9fc07ae1f0fe4671c
TabouyT <timothee.tabouy@gmail.com>
2017-09-08 14:10:45 +0200

    mise en place VEM

--- R/SBM_collection-R6Class.R
+++ R/SBM_collection-R6Class.R
@@ -8,6 +8,7 @@ SBM_collection <-
       vBlocks  = NULL, # the vector of number of blocks considered in the collection
       models   = NULL, # the collection of SBM fit
       sampling = NULL, # the sampling design for missing data modeling
+      samplingData = NULL, # informations about the sampling
       family   = NULL, # the emission law of the adjacency matrix
       link     = NULL  # directed or not, depends on what we are modeling
       ## methods
@@ -22,13 +23,14 @@ SBM_collection$set("public", "initialize",
     self$link     <- link
     self$vBlocks  <- vBlocks
     self$sampledNetwork <- sampledNetwork$new(sampledNetwork, link)
-    self$sampling       <- switch(sampling,
-                            "doubleStandard" = sampling_doubleStandard(self$sampledNetwork$nNodes, c(1/2, 1/2), link),
-                            "class"          = ,
-                            "starDegree"     = ,
-                            "MAREdge"        = ,
-                            "MARNode"        = ,
-                            "snowball"       =)
+    self$samplingData       <- switch(sampling,
+                            "doubleStandard" = sampling_doubleStandard$new(self$sampledNetwork$nNodes, NA, link),
+                            "class"          = sampling_class$new(self$sampledNetwork$nNodes, NA, link),
+                            "starDegree"     = sampling_starDegree$new(self$sampledNetwork$nNodes, NA, link),
+                            "MAREdge"        = sampling_randomPairMAR$new(self$sampledNetwork$nNodes, NA, link),
+                            "MARNode"        = sampling_randomNodeMAR$new(self$sampledNetwork$nNodes, NA, link),
+                            "snowball"       = sampling_snowball$new(self$sampledNetwork$nNodes, NA, link))
+    self$sampling <- sampling
   
   }
 )
@@ -36,9 +38,6 @@ SBM_collection$set("public", "initialize",
 SBM_collection$set("public", "estimate",
   function() {
 
-    n <- nrow(X)
-    miss <- which(is.na(X))
-    obs  <- which(!is.na(X))
     ## A. INITIALISATION DES PARAMÈTRES
     
     ## A.1 PARAMÈTRES CONTRÔLANT L'OPTIMISATION
@@ -48,39 +47,46 @@ SBM_collection$set("public", "estimate",
     maxIter.FP <- 5
     mc.cores   <- mc.cores
     zero       <- .Machine$double.eps
-    if(is.character(cl.init)) stopifnot(cl.init %in% c("mar.CAH","CAH", "spectral", "spectral.CAH"))
-    
+
     ## A.2 PARAMÈTRES FIXES EN COURS D'ALGORITHME (données)
-    R  <- 1*(!is.na(X)); diag(R) <- 1         # matrix of observed data
-    X1 <- X; X1[is.na(X)] <- 0                # indicator for truly observed edges
-    # X0 <- (1-X1); diag(X0) <- 0; X0 <- X0 * R # indicator for truly unobserved edges
-    X2 <- X
+
+    SBM <- switch(paste0("SBM_",self$family, self$link),
+                  "SBM_bernoulliUndirected" = SBM_bernoulliUndirected$new(self$sampling$nNodes, NA, NA),
+                  "SBM_bernoulliDirected"   = SBM_bernoulliDirected$new(self$sampling$nNodes, NA, NA),
+                  "SBM_poissonUndirected"   = SBM_poissonUndirected$new(self$sampling$nNodes, NA, NA),
+                  "SBM_poissonDirected"     = SBM_poissonDirected$new(self$sampling$nNodes, NA, NA))
+    
+
+    VEMfit <- SBM_VEMfit$new(, NA, NA, SBM, lowerBound, vICL, blocks)
+
+    
     
     self$models <- mclapply(self$vBlocks, function(Q) {
       ## ===================================================================
       ## B. VEM À NOMBRE DE CLASSES Q FIXÉ
       
       ## B.1 INITIALISATION DES CLUSTERS
-      if(is.null(CL)){
-        if (is.character(cl.init) | is.null(cl.init)) { ## CAH, spectral clustering or user defined
-          cl0 <- switch(cl.init,
-                        "spectral"     = SpectralClustering(X, Q),
-                        "CAH"          = graphCAH(X, Q),
-                        "mar.spectral" = SpectralClustering(X, Q),
-                        "mar.CAH"      = graphCAH(X, Q))
-        } else {
-          if (is.list(cl.init)) {
-            cl0 <- cl.init[[Q]]
-          } else {
-            cl0 <- cl.init[[which(seq.Q == Q)]]
-          }
-        }
-      } else {
-        cl0 <- CL
-      }
-      if(!is.null(CL.init) & is.null(CL)){
-        cl0 <- CL.init
-      }
+      # if(is.null(CL)){
+      #   if (is.character(cl.init) | is.null(cl.init)) { ## CAH, spectral clustering or user defined
+      #     cl0 <- switch(cl.init,
+      #                   "spectral"     = SpectralClustering(X, Q),
+      #                   "CAH"          = graphCAH(X, Q),
+      #                   "mar.spectral" = SpectralClustering(X, Q),
+      #                   "mar.CAH"      = graphCAH(X, Q))
+      #   } else {
+      #     if (is.list(cl.init)) {
+      #       cl0 <- cl.init[[Q]]
+      #     } else {
+      #       cl0 <- cl.init[[which(seq.Q == Q)]]
+      #     }
+      #   }
+      # } else {
+      #   cl0 <- CL
+      # }
+      # if(!is.null(CL.init) & is.null(CL)){
+      #   cl0 <- CL.init
+      # }
+      
       
       Tau      <- matrix(0,n,Q); Tau[cbind(1:n, cl0)] <- 1
       piInitNu <- (t(Tau)%*% X1 %*%Tau) / (t(Tau)%*%((1-diag(n)))%*%Tau)
@@ -146,27 +152,10 @@ SBM_collection$set("public", "estimate",
       
       ICL <- -2*logLikFull + (2 + Q*(Q+1)/2)*log(n*(n-1)/2) + (Q-1)*log(n)
       
-      return(new(Class = "SBM.fit",
-                 theta        = theta[1:i],
-                 psi          = psi.all[1:i],
-                 Q            = as.integer(Q),
-                 net          = X2,
-                 cl           = lapply(Tau.all[1:i], function(Tau) factor(as.numeric(apply(Tau, 1, which.max)), levels = 1:Q)),
-                 crit         = J[1:i],
-                 conv         = conv[1:i],
-                 ICL          = ICL
-                 
-      ))
+      return()
     }, mc.cores=mc.cores)
     
-    return(new(Class = "missSBM",
-               missingness = "twoStd",
-               algorithm   = "Variational Expectation-Maximization",
-               obs.rate    = sum(!is.na(X))/length(X),
-               network     = Matrix(X),
-               ICLs        = setNames(sapply(models, function(model) model@ICL),as.character(seq.Q)),
-               models      = models
-    ))
+    return()
     
   }
 )
