feba055a7c90687708c82c416c094cb645c235e5
Julien Chiquet <julien.chiquet@gmail.com>
2018-03-05 09:15:38 +0100

    added esr1 data set to the package

---
+++ R/net_esr1.R
@@ -0,0 +1,13 @@
+#' ESR ego centred network
+#'
+#' A dataset containing the weigthed PPI network centred around ESR1 and extracted from string data base
+#' diamonds.
+#'
+#' @format A sparse symmetric matrix with 741 rows and 741 columns:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{https://string-db.org/}
+"net_esr1"
