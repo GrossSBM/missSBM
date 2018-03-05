98ee2b02a41e365c61d5ce4780b9ac84e2a25c9b
Julien Chiquet <julien.chiquet@gmail.com>
2018-03-05 15:00:07 +0100

    setting default initialisation to hirearchical clustering

--- R/missingSBM.R
+++ R/missingSBM.R
@@ -128,7 +128,7 @@ samplingSBM <- function(adjacencyMatrix, sampling, parameters, clusters = NULL){
 #'
 #' @import R6
 #' @export
-inferSBM <- function(adjacencyMatrix, vBlocks, sampling, clusterInit = "spectral"){
+inferSBM <- function(adjacencyMatrix, vBlocks, sampling, clusterInit = "hierarchical"){
 
   sampledNet <- sampledNetwork$new(adjacencyMatrix)
   cat("\n")
