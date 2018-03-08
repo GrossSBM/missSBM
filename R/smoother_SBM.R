17a9e6bb16e4d82761048dde46b04c56cd2c2f8d
tabouy <timothee.tabouy@agroparistech.fr>
2018-03-08 19:31:05 +0100

    smoothing_SBM

--- R/smoother_SBM.R
+++ R/smoother_SBM.R
@@ -1,4 +1,4 @@
-
+#' @export
 smoothingBackward <- function(models, vBlocks, sampledAdjMatrix, sampling, mc.cores = 4) {
   sampledNet <- sampledNetwork$new(sampledAdjMatrix)
   for (i in rev(vBlocks[-1])) {
@@ -14,14 +14,18 @@ smoothingBackward <- function(models, vBlocks, sampledAdjMatrix, sampling, mc.co
       }, mc.cores = mc.cores)
       vICLs <- sapply(candidates, function(candidate) candidate$vICL)
       best_one <- candidates[[which.min(vICLs)]]
-      if (best_one$vICL < models[[i - 1]]$vICL)
+      if(is.na(models[[i - 1]]$vICL)){
         models[[i - 1]] <- best_one
+      } else if(best_one$vICL < models[[i - 1]]$vICL) {
+        models[[i - 1]] <- best_one
+      }
     }
   }
   models
 }
 
-smoothingForward_half <- function(models, vBlocks, sampledAdjMatrix, sampling, mc.cores = 3) {
+#' @export
+smoothingForward_half <- function(models, vBlocks, sampledAdjMatrix, sampling, mc.cores = 4) {
   sampledNet <- sampledNetwork$new(sampledAdjMatrix)
   for(i in vBlocks[-length(vBlocks)]){
     cl_split <- factor(models[[i]]$fittedSBM$memberships)
@@ -43,6 +47,7 @@ smoothingForward_half <- function(models, vBlocks, sampledAdjMatrix, sampling, m
   models
 }
 
+#' @export
 smoothingForward_SpCl <- function(models, vBlocks, sampledAdjMatrix, sampling, mc.cores = 4) {
   sampledNet <- sampledNetwork$new(sampledAdjMatrix)
   for(i in vBlocks[-length(vBlocks)]){
@@ -50,8 +55,9 @@ smoothingForward_SpCl <- function(models, vBlocks, sampledAdjMatrix, sampling, m
     levels(cl_split) <- c(levels(cl_split), as.character(i+1))
     if (nlevels(cl_split) - 1 == i) {
       candidates <- pbmclapply(1:i, function(j) {
+        # if(i==6) browser()
         cl <- as.numeric(cl_split); indices <- which(cl==j)
-        if(length(cl[indices]) > 1){
+        if(length(cl[indices]) > 10){
           cut <- as.numeric(init_spectral(sampledAdjMatrix[indices, indices],2))
           cl[which(cl==j)][which(cut==1)] <- j; cl[which(cl==j)][which(cut==2)] <- i + 1
           model <- missingSBM_fit$new(sampledNet, i + 1, sampling, cl)
@@ -63,13 +69,18 @@ smoothingForward_SpCl <- function(models, vBlocks, sampledAdjMatrix, sampling, m
       }, mc.cores = mc.cores)
       vICLs <- sapply(candidates, function(candidate) candidate$vICL)
       best_one <- candidates[[which.min(vICLs)]]
-      if (best_one$vICL < models[[i + 1]]$vICL)
+      if(is.na(models[[i + 1]]$vICL)){
         models[[i + 1]] <- best_one
+      } else if(best_one$vICL < models[[i + 1]]$vICL) {
+        models[[i + 1]] <- best_one
+      }
     }
   }
+
   models
 }
 
+#' @export
 smoothingForBackWard_half <- function(models, vBlocks, sampledAdjMatrix, sampling, nIter = 2){
   out <- models
   for (i in 1:nIter) {
@@ -79,7 +90,8 @@ smoothingForBackWard_half <- function(models, vBlocks, sampledAdjMatrix, samplin
   out
 }
 
-smoothingForBackWard_SpCl <- function(models, vBlocks, sampledAdjMatrix, sampling, nIter = 1){
+#' @export
+smoothingForBackWard_SpCl <- function(models, vBlocks, sampledAdjMatrix, sampling, nIter = 2){
   out <- models
   for (i in 1:nIter) {
     out <- smoothingBackward(out, vBlocks, sampledAdjMatrix,sampling)
@@ -87,37 +99,3 @@ smoothingForBackWard_SpCl <- function(models, vBlocks, sampledAdjMatrix, samplin
   }
   out
 }
-
-#### TESTS ####
-
-# rm(list=ls())
-# library(missSBM)
-# library(pbmcapply)
-# ## Model :
-# n <- 300
-# Q <- 3
-# pi <- diag(0.45,Q) + .05
-# alpha <- rep(1,Q)/Q
-# family <- "Bernoulli"
-# directed <- FALSE
-# mySBM <- simulateSBM(n, alpha, pi, directed)
-# adjacencyMatrix <- mySBM$adjacencyMatrix
-# samplingParameters <- .7
-# sampling <- "dyad"
-# sampSBM <- samplingSBM(adjacencyMatrix, sampling, samplingParameters)
-# sampledAdjMatrix <- sampSBM$adjacencyMatrix
-# vBlocks <- 1:10
-# sbm <- inferSBM(sampledAdjMatrix, vBlocks, "dyad")
-# smoothed_fwrd_half <- smoothingForward_half(sbm$models, vBlocks, sampledAdjMatrix, sampling)
-# smoothed_fwrd_SpCl <- smoothingForward_SpCl(sbm$models, vBlocks, sampledAdjMatrix, sampling)
-# smoothed_back      <- smoothingBackward(sbm$models, vBlocks, sampledAdjMatrix, sampling)
-# smoothed_fb_half   <- smoothingForBackWard_half(sbm$models, vBlocks, sampledAdjMatrix, sampling)
-# smoothed_fb_SpCl   <- smoothingForBackWard_SpCl(sbm$models, vBlocks, sampledAdjMatrix,sampling)
-
-# vICL           <- sapply(sbm$models, function(model) model$vICL); plot(vICL, type = "l")
-# vICL_back      <- sapply(smoothed_back, function(model) model$vICL); lines(vICL_back, col="blue")
-# vICL_fwrd_half <- sapply(smoothed_fwrd_half, function(model) model$vICL); lines(vICL_fwrd_half, col="red")
-# vICL_fwrd_SpCl <- sapply(smoothed_fwrd_SpCl, function(model) model$vICL); lines(vICL_fwrd_SpCl, col="yellow")
-# vICL_fb_half   <- sapply(smoothed_fb_half, function(model) model$vICL); lines(vICL_fb_half  , col="orange")
-# vICL_fb_SpCl   <- sapply(smoothed_fb_SpCl, function(model) model$vICL); lines(vICL_fb_SpCl  , col="green")
-
---
+++ inst/test_smoothing.R
@@ -0,0 +1,32 @@
+#### TESTS ####
+# rm(list=ls())
+# library(missSBM)
+library(pbmcapply)
+
+n <- 200
+Q <- 3
+pi <- diag(0.45,Q) + .05
+alpha <- rep(1,Q)/Q
+family <- "Bernoulli"
+directed <- FALSE
+mySBM <- simulateSBM(n, alpha, pi, directed)
+adjacencyMatrix <- mySBM$adjacencyMatrix
+samplingParameters <- .5
+sampling <- "dyad"
+sampSBM <- samplingSBM(adjacencyMatrix, sampling, samplingParameters)
+sampledAdjMatrix <- sampSBM$adjacencyMatrix
+vBlocks <- 1:10
+sbm <- inferSBM(sampledAdjMatrix, vBlocks, "dyad")
+
+# smoothed_fwrd_half <- smoothingForward_half(sbm$models, vBlocks, sampledAdjMatrix, sampling)
+smoothed_fwrd_SpCl <- smoothingForward_SpCl(sbm$models, vBlocks, sampledAdjMatrix, sampling)
+# smoothed_back      <- smoothingBackward(sbm$models, vBlocks, sampledAdjMatrix, sampling)
+# smoothed_fb_half   <- smoothingForBackWard_half(sbm$models, vBlocks, sampledAdjMatrix, sampling)
+# smoothed_fb_SpCl   <- smoothingForBackWard_SpCl(sbm$models, vBlocks, sampledAdjMatrix,sampling)
+
+vICL           <- sapply(sbm$models, function(model) model$vICL); plot(vICL, type = "l")
+# vICL_back      <- sapply(smoothed_back, function(model) model$vICL); lines(vICL_back, col="blue")
+# vICL_fwrd_half <- sapply(smoothed_fwrd_half, function(model) model$vICL); lines(vICL_fwrd_half, col="red")
+# vICL_fwrd_SpCl <- sapply(smoothed_fwrd_SpCl, function(model) model$vICL); lines(vICL_fwrd_SpCl, col="yellow")
+# vICL_fb_half   <- sapply(smoothed_fb_half, function(model) model$vICL); lines(vICL_fb_half  , col="orange")
+# vICL_fb_SpCl   <- sapply(smoothed_fb_SpCl, function(model) model$vICL); lines(vICL_fb_SpCl  , col="green")
