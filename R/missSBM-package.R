e9a722481113e1c18ed0fede0e799f9922f9107f
Julien Chiquet <julien.chiquet@gmail.com>
2018-06-29 17:45:40 +0200

    adde support for RcppArmadillo

--- R/missSBM-package.R
+++ R/missSBM-package.R
@@ -20,5 +20,7 @@
 #' @references [1] Tabouy et al., Variationnal inference of Stochastic Block Model from sampled data (2017). arXiv:1707.04141.
 #' @references [2] Kolaczyk Erik D. (2009). Statistical Analysis of Network Data: Methods and Models. Springer.
 #' @import R6
+#' @useDynLib missSBM
+#' @importFrom Rcpp sourceCpp
 #' @name missSBM
 NULL
---
+++ src/roundProduct.cpp
@@ -0,0 +1,20 @@
+#include "RcppArmadillo.h"
+
+// [[Rcpp::depends(RcppArmadillo)]]
+
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+Rcpp::NumericMatrix roundProduct(arma::cube X, arma::vec beta) {
+
+  int N = X.n_rows;
+  arma::mat M(N,N);
+
+  for(int i=0; i<N; i++) {
+      for (int j=0; j<N; j++) {
+        arma::vec param = X.tube(i,j);
+        M(i,j) = as_scalar(beta.t()*param);
+      }
+  }
+  return Rcpp::wrap(M);
+}
