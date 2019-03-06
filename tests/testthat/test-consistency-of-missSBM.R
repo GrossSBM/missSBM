4d8d5a4722293ead5cdedcc8ad9500ff5ecb4d75
Julien Chiquet <julien.chiquet@inra.fr>
2019-03-06 13:51:56 +0100

    completing test for consistency of missSBM compared to Tim's code (dyad, node, block and double standard sampling are ok)

---
+++ tests/testthat/test-consistency-of-missSBM.R
@@ -0,0 +1,97 @@
+context("test-consistency-of-misssbm-with-dyad-sampling")
+
+library(aricode)
+
+referenceResults <- readRDS(system.file("extdata", "referenceResults.rds", package = "missSBM"))
+truth   <- referenceResults$true_sbm
+
+tol_ref   <- 1e-3
+tol_truth <- 1e-2
+tol_ARI   <- .9
+error <- function(beta1, beta2) {
+  sum((sort(beta1) - sort(beta2))^2)/truth$Q
+}
+
+test_that("check consistency against Tim's code for dyad, node, double standard and block sampling", {
+
+
+  for (sampling in c("dyad", "node", "twoStd", "block")) {
+
+    refAlgo <- referenceResults[[sampling]]
+
+    missSBM_out <- missSBM::inferSBM(
+      adjacencyMatrix = refAlgo$Y,
+      vBlocks = truth$Q,
+      sampling = ifelse(sampling == "twoStd", "double_standard", sampling),
+      trace = FALSE
+    )
+    newAlgo <- missSBM_out[[1]]
+
+    ## alpha
+    expect_lt(error(sort(newAlgo$fittedSBM$mixtureParam), sort(refAlgo$alpha)), tol_ref)
+    expect_lt(error(sort(newAlgo$fittedSBM$mixtureParam), sort(truth$alpha)), tol_truth)
+    expect_lt(error(sort(refAlgo$alpha), sort(truth$alpha)), tol_truth)
+
+    ## pi
+    expect_lt(error(newAlgo$fittedSBM$connectParam, refAlgo$pi), tol_ref)
+    expect_lt(error(newAlgo$fittedSBM$connectParam, truth$pi), tol_truth)
+    expect_lt(error(refAlgo$pi, truth$pi), tol_truth)
+
+    ## clustering
+    expect_gt(ARI(newAlgo$fittedSBM$memberships, refAlgo$cl), tol_ARI)
+    expect_gt(ARI(newAlgo$fittedSBM$memberships, truth$cl), tol_truth)
+    expect_gt(ARI(refAlgo$cl, truth$cl), tol_truth)
+
+    if (!(sampling %in% c("dyad", "node"))) {
+      ## psi
+      expect_lt(error(sort(newAlgo$fittedSampling$parameters), sort(refAlgo$psi_chap)), tol_ref)
+      expect_lte(
+        error(sort(newAlgo$fittedSampling$parameters), sort(refAlgo$psi_true)),
+        error(sort(refAlgo$psi_chap), sort(refAlgo$psi_true)),
+        tol_ref
+      )
+    }
+  }
+})
+
+# test_that("check consistency against Tim's code for dyad and node sampling with covariates", {
+#
+#
+#   for (sampling in c("covDyad", "covNode")) {
+#
+#     refAlgo <- referenceResults[[sampling]]
+#
+#     missSBM_out <- missSBM::inferSBM(
+#       adjacencyMatrix = refAlgo$Y,
+#       vBlocks = truth$Q,
+#       sampling = ifelse(sampling == "covDyad", "dyad", "node"),
+#       trace = FALSE
+#     )
+#     newAlgo <- missSBM_out[[1]]
+#
+#     ## alpha
+#     expect_lt(error(sort(newAlgo$fittedSBM$mixtureParam), sort(refAlgo$alpha)), tol_ref)
+#     expect_lt(error(sort(newAlgo$fittedSBM$mixtureParam), sort(truth$alpha)), tol_truth)
+#     expect_lt(error(sort(refAlgo$alpha), sort(truth$alpha)), tol_truth)
+#
+#     ## pi
+#     expect_lt(error(newAlgo$fittedSBM$connectParam, refAlgo$pi), tol_ref)
+#     expect_lt(error(newAlgo$fittedSBM$connectParam, truth$pi), tol_truth)
+#     expect_lt(error(refAlgo$pi, truth$pi), tol_truth)
+#
+#     ## clustering
+#     expect_gt(ARI(newAlgo$fittedSBM$memberships, refAlgo$cl), tol_ARI)
+#     expect_gt(ARI(newAlgo$fittedSBM$memberships, truth$cl), tol_truth)
+#     expect_gt(ARI(refAlgo$cl, truth$cl), tol_truth)
+#
+#     if (!(sampling %in% c("dyad", "node"))) {
+#       ## psi
+#       expect_lt(error(sort(newAlgo$fittedSampling$parameters), sort(refAlgo$psi_chap)), tol_ref)
+#       expect_lte(
+#         error(sort(newAlgo$fittedSampling$parameters), sort(refAlgo$psi_true)),
+#         error(sort(refAlgo$psi_chap), sort(refAlgo$psi_true)),
+#         tol_ref
+#       )
+#     }
+#   }
+# })
