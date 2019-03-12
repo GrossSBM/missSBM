06922a05ee309ff250c267fa1b43b6a336c0c774
Julien Chiquet <julien.chiquet@gmail.com>
2019-03-12 14:47:26 +0100

    regenerating timothee references outputs

--- tests/testthat/test-TOP-LEVEL-consistency.R
+++ tests/testthat/test-TOP-LEVEL-consistency.R
@@ -7,82 +7,135 @@ referenceResults <- readRDS(system.file("extdata", "referenceResults.rds", packa
 
 test_that("check consistency against Tim's code for dyad, node, double standard and block sampling", {
 
-  tol_ref   <- 1e-3
-  tol_truth <- 5e-2
-  tol_ARI   <- .9
+  tol_ref   <- 1e-2
+  tol_truth <- 5e-3
+  tol_ARI   <- .8
   truth   <- referenceResults$true_sbm
 
+  cat("\nsampling: ")
   for (sampling in c("dyad", "node", "double-standard", "block-node")) {
 
+    # tol_ref   <- 1e-3 ok for double standard
+    # tol_truth <- 1e-3
+
     refAlgo <- referenceResults[[sampling]]
+    cat(sampling)
 
     missSBM_out <- missSBM::missSBM(
-      adjacencyMatrix = refAlgo$Y,
-      vBlocks = truth$Q,
+      adjacencyMatrix = refAlgo$sampledNet,
+      vBlocks = truth$nBlocks,
       sampling = sampling,
-      trace = FALSE
+      trace = FALSE,
+      clusterInit = "spectral"
     )
     newAlgo <- missSBM_out[[1]]
 
-    ## alpha
-    expect_lt(error(newAlgo$fittedSBM$mixtureParam, refAlgo$alpha, sort = TRUE), tol_ref)
-    expect_lt(error(newAlgo$fittedSBM$mixtureParam, truth$alpha, sort = TRUE), tol_truth)
-    expect_lt(error(refAlgo$alpha, truth$alpha, sort = TRUE), tol_truth)
+    ## mixture parameters (alpha)
+    err_new <- error(newAlgo$fittedSBM$mixtureParam, truth$mixtureParam, sort = TRUE)
+    err_old <- error(refAlgo$mixtureParam          , truth$mixtureParam, sort = TRUE)
+    gap_old <- error(newAlgo$fittedSBM$mixtureParam, refAlgo$mixtureParam, sort = TRUE)
+    if (err_new < err_old) {
+      expect_lt(err_new, tol_truth)
+      cat(" new better on mixture")
+    } else {
+      expect_lt(gap_old, tol_ref)
+      expect_lt(gap_old, tol_ref)
+    }
 
-    ## pi
-    expect_lt(error(newAlgo$fittedSBM$connectParam, refAlgo$pi), tol_ref)
-    expect_lt(error(newAlgo$fittedSBM$connectParam, truth$pi), tol_truth)
-    expect_lt(error(refAlgo$pi, truth$pi), tol_truth)
+    ## connectivity parameters (pi)
+    err_new <- error(newAlgo$fittedSBM$connectParam, truth$connectParam, sort = TRUE)
+    err_old <- error(refAlgo$connectParam          , truth$connectParam, sort = TRUE)
+    err_gap <- error(newAlgo$fittedSBM$connectParam, refAlgo$connectParam, sort = TRUE)
+    if (err_new < err_old) {
+      expect_lt(err_new, tol_truth)
+      cat(" new better on connectivity")
+    } else {
+      expect_lt(gap_old, tol_ref)
+      expect_lt(gap_old, tol_ref)
+    }
 
     ## clustering
-    expect_gt(ARI(newAlgo$fittedSBM$memberships, refAlgo$cl), tol_ARI)
-    expect_gt(ARI(newAlgo$fittedSBM$memberships, truth$cl), tol_truth)
-    expect_gt(ARI(refAlgo$cl, truth$cl), tol_truth)
+    ARI_new <- ARI(newAlgo$fittedSBM$memberships, truth$memberships)
+    ARI_old <- ARI(refAlgo$memberships, truth$memberships)
+    ARI_gap <- ARI(refAlgo$memberships, newAlgo$fittedSBM$memberships)
+    expect_gt(ARI_new, tol_ARI)
+    expect_gt(ARI_old, tol_ARI)
+    expect_gt(ARI_gap, tol_ARI)
 
     if (!(sampling %in% c("dyad", "node"))) {
       ## psi
-      expect_lt(error(newAlgo$fittedSampling$parameters, refAlgo$psi_chap, sort = TRUE), tol_ref)
-      expect_lt(error(newAlgo$fittedSampling$parameters, refAlgo$psi_vrai, sort = TRUE), tol_truth)
-      expect_lt(error(refAlgo$psi_chap, refAlgo$psi_vrai, sort = TRUE), tol_truth)
+      err_new  <- error(newAlgo$fittedSampling$parameters, refAlgo$true_samplingParam, sort = TRUE)
+      err_old  <- error(refAlgo$samplingParam            , refAlgo$true_samplingParam, sort = TRUE)
+      err_gap  <- error(newAlgo$fittedSampling$parameters, refAlgo$samplingParam, sort = TRUE)
+      if (err_new < err_old) {
+        expect_lt(err_new, tol_truth)
+        cat(" new better on sampling parameters")
+      } else {
+        expect_lt(gap_old, tol_ref)
+        expect_lt(gap_old, tol_ref)
+      }
     }
+    cat("\n")
   }
 })
 
 test_that("check consistency against Tim's code for dyad and node sampling with covariates", {
 
-  truth   <- referenceResults$true_sbm_wCov
+  truth   <- referenceResults$true_sbm_cov
   tol_ref   <- 1e-2
   tol_truth <- 1e-2
   tol_ARI   <- .7
 
-  for (sampling in c("covDyad", "covNode")) {
+  for (sampling in c("dyad-covariates", "node-covariates")) {
 
     refAlgo <- referenceResults[[sampling]]
 
     missSBM_out <- missSBM::missSBM(
-      adjacencyMatrix = refAlgo$Y,
-      vBlocks = truth$Q,
-      sampling = ifelse(sampling == "covDyad", "dyad", "node"),
+      adjacencyMatrix = refAlgo$sampledNet,
+      vBlocks = truth$nBlocks,
+      sampling = ifelse(sampling == "dyad-covariates", "dyad", "node"),
       trace = TRUE,
-      covarMatrix = truth$covMatrix,
+      covarMatrix = refAlgo$covarMatrix
     )
     newAlgo <- missSBM_out[[1]]
 
-    ## alpha
-    expect_lt(error(newAlgo$fittedSBM$mixtureParam, refAlgo$alpha, sort = TRUE), tol_ref)
-    expect_lt(error(newAlgo$fittedSBM$mixtureParam, truth$alpha, sort = TRUE), tol_truth)
-    ## refAlgo is poor for alpha and clsutering, but good for estimating gamma !!
-    expect_lt(error(refAlgo$alpha, truth$alpha, sort = TRUE), tol_truth)
+    ## mixture parameters (alpha)
+    err_new <- error(newAlgo$fittedSBM$mixtureParam, truth$mixtureParam, sort = TRUE)
+    err_old <- error(refAlgo$mixtureParam          , truth$mixtureParam, sort = TRUE)
+    gap_old <- error(newAlgo$fittedSBM$mixtureParam, refAlgo$mixtureParam, sort = TRUE)
+    if (err_new < err_old) {
+      expect_lt(err_new, tol_truth)
+      cat(" new better on mixture")
+    } else {
+      expect_lt(gap_old, tol_ref)
+      expect_lt(gap_old, tol_ref)
+    }
+
+    ## connectivity parameters (pi)
+    err_new <- error(newAlgo$fittedSBM$connectParam, truth$connectParam, sort = TRUE)
+    err_old <- error(refAlgo$connectParam          , truth$connectParam, sort = TRUE)
+    err_gap <- error(newAlgo$fittedSBM$connectParam, refAlgo$connectParam, sort = TRUE)
+    if (err_new < err_old) {
+      expect_lt(err_new, tol_truth)
+      cat(" new better on connectivity")
+    } else {
+      expect_lt(gap_old, tol_ref)
+      expect_lt(gap_old, tol_ref)
+    }
+
+    ## clustering
+    ARI_new <- ARI(newAlgo$fittedSBM$memberships, truth$memberships)
+    ARI_old <- ARI(refAlgo$memberships, truth$memberships)
+    ARI_gap <- ARI(refAlgo$memberships, newAlgo$fittedSBM$memberships)
+    expect_gt(ARI_new, tol_ARI)
+    expect_gt(ARI_old, tol_ARI)
+    expect_gt(ARI_gap, tol_ARI)
 
     ## PI
     # expect_lt(error(newAlgo$fittedSBM$connectProb, refAlgo$pi), tol_ref)
     # expect_lt(error(logistic(newAlgo$fittedSBM$connectParam), missSBM:::logistic(truth$gamma)), tol_truth)
     # expect_lt(error(logistic(refAlgo$gamma_covDyad), missSBM:::logistic(truth$gamma)), tol_truth)
 
-    ## clustering
-##    expect_gt(ARI(newAlgo$fittedSBM$memberships, refAlgo$cl), tol_ARI)
-    expect_gt(ARI(newAlgo$fittedSBM$memberships, truth$cl), tol_ARI)
-   ##  expect_gt(ARI(refAlgo$cl, truth$cl), tol_ARI)
 
   }
 })
