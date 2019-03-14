d891086aeddd6a275f3e26a78cc064ed728a8e46
Julien Chiquet <julien.chiquet@inra.fr>
2019-03-14 18:24:08 +0100

    renaming ESR1 data set and giving up with formating PB's dataset

---
+++ R/ESR1.R
@@ -0,0 +1,7 @@
+#' ESR ego centred network
+#'
+#' A dataset containing the weigthed PPI network centred around ESR1
+#'
+#' @format A sparse symmetric matrix with 741 rows and 741 columns \code{net_ER}
+#' @source \url{https://string-db.org/}
+"ESR1"
--- R/data.R
+++
@@ -1,7 +0,0 @@
-#' ESR ego centred network
-#'
-#' A dataset containing the weigthed PPI network centred around ESR1
-#'
-#' @format A sparse symmetric matrix with 741 rows and 741 columns \code{net_ER}
-#' @source \url{https://string-db.org/}
-"net_ER"
