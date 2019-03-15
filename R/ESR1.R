74efba73d8a0d5dce57a2a57f2c5a997b7e4dfc5
Julien Chiquet <julien.chiquet@inra.fr>
2019-03-15 08:35:11 +0100

    only retaining ESR1 data for the moment (wwatingin for branch networkData improuvment and push by Tim

--- R/ESR1.R
+++ R/ESR1.R
@@ -2,6 +2,6 @@
 #'
 #' A dataset containing the weigthed PPI network centred around ESR1
 #'
-#' @format A sparse symmetric matrix with 741 rows and 741 columns \code{net_ER}
+#' @format A sparse symmetric matrix with 741 rows and 741 columns \code{ESR1}
 #' @source \url{https://string-db.org/}
 "ESR1"
