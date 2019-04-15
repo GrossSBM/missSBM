14e22dfe14ce96fd1052fbc9ac2a944a80487131
barbillon <pierre.barbillon@agroparistech.fr>
2019-04-15 15:04:46 -0400

    ex cov prob

--- inst/covariates.R
+++ inst/covariates.R
@@ -4,23 +4,26 @@ library(igraph)
 
 data(war)
 beligerent_adjacency <- as_adj(war$beligerent, sparse = FALSE)
-
+nWar = nrow(beligerent_adjacency)
+matsumpower = matrix(war$beligerent$power,nrow = nWar,ncol = nWar) + matrix(war$beligerent$power,nrow = nWar,ncol = nWar,byrow=T)
+diag(matsumpower) = 0 # diagonal has to be set to 0
+sampleNet_cov <- prepare_data(beligerent_adjacency, covariates = list(matsumpower))
 
 vBlocks <- 1:5
 collection_sbm_cov_full <- estimate(sampleNet_cov, vBlocks = vBlocks, sampling = "dyad")
 res_unsmoothed <- data.frame(
   ICL     = collection_sbm_cov_full$ICL,
-  nBlocks = vBlocks, 
+  nBlocks = vBlocks,
   type    = "raw"
 )
-smooth(collection_sbm_cov_full, "both", control = list(iterates = 2)) 
+smooth(collection_sbm_cov_full, "both", control = list(iterates = 2))
 res_smoothed <- data.frame(
   ICL     = collection_sbm_cov_full$ICL,
-  nBlocks = vBlocks, 
+  nBlocks = vBlocks,
   type    = "smoothed"
 )
-rbind(res_unsmoothed, res_smoothed) %>% 
-  ggplot(aes(x = nBlocks, y = ICL, group = type, color = type)) + 
+rbind(res_unsmoothed, res_smoothed) %>%
+  ggplot(aes(x = nBlocks, y = ICL, group = type, color = type)) +
   geom_line() + theme_bw()
 
 # this is working well
