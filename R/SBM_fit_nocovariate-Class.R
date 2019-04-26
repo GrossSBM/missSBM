c4fff4e3b32c7cd3c28ae4362abced97811b6a58
jchiquet <julien.chiquet@gmail.com>
2019-04-26 17:23:07 +0200

    back to clean code in update_block

--- R/SBM_fit_nocovariate-Class.R
+++ R/SBM_fit_nocovariate-Class.R
@@ -37,13 +37,7 @@ R6::R6Class(classname = "SBM_fit_nocovariate",
           ## Bernoulli directed
           tau <- tau + t(private$Y) %*% private$tau %*% t(log(t(private$pi))) + t(adjMatrix_bar) %*% private$tau %*% t(log(1 - t(private$pi)))
         }
-        temp = t(apply(sweep(tau, 2, log(private$alpha), "+"), 1, .softmax))
-        thr = 1e-4
-        temp[temp<thr] = thr
-        temp[temp>1-thr] = 1-thr
-        temp = temp / matrix(rowSums(temp),nrow(temp),ncol(temp),byrow=F)
-        private$tau = temp
-        #private$tau <- t(apply(sweep(tau, 2, log(private$alpha), "+"), 1, .softmax))
+        private$tau <- t(apply(sweep(tau, 2, log(private$alpha), "+"), 1, .softmax))
       }
     }
   ),
