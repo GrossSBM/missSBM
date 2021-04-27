0bb78a43ade946ec9f5e759632a679e4ad9577df
Julien Chiquet <julien.chiquet@gmail.com>
2021-04-27 22:18:30 +0200

    correcting links and spelling to pass check without warning

--- R/R6Class-missSBM_fit.R
+++ R/R6Class-missSBM_fit.R
@@ -12,7 +12,7 @@
 #' ## Sample 75% of dyads in  French political Blogosphere's network data
 #' adjMatrix <- missSBM::frenchblog2007 %>%
 #'   igraph::as_adj (sparse = FALSE) %>%
-#'   missSBM::observeNetwork(sampling = "dyad", parameters = 0.25)
+#'   missSBM::observeNetwork(sampling = "dyad", parameters = 0.75)
 #' collection <- estimateMissSBM(adjMatrix, 3:5, sampling = "dyad")
 #' my_missSBM_fit <- collection$bestModel
 #' class(my_missSBM_fit)
